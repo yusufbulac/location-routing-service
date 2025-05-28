aaebdf0a05c4e5412636fdbb7e2e560e0f250994
yusufbulac <ybulac@gmail.com>
2025-05-28 11:22:36 +0300

    created location model

---
+++ internal/model/location.go
@@ -0,0 +1,9 @@
+package model
+
+type Location struct {
+	ID        uint    `gorm:"primaryKey" json:"id"`
+	Name      string  `gorm:"type:varchar(100);not null" json:"name"`
+	Latitude  float64 `gorm:"not null" json:"latitude"`
+	Longitude float64 `gorm:"not null" json:"longitude"`
+	Color     string  `gorm:"type:char(7);not null" json:"color"`
+}
