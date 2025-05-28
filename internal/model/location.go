31166b2fe959eefd5344e5ea361f3442f435c562
yusufbulac <ybulac@gmail.com>
2025-05-28 13:47:27 +0300

    added timestamp for location model

--- internal/model/location.go
+++ internal/model/location.go
@@ -1,9 +1,14 @@
 package model
 
+import "time"
+
 type Location struct {
 	ID        uint    `gorm:"primaryKey" json:"id"`
 	Name      string  `gorm:"type:varchar(100);not null" json:"name"`
 	Latitude  float64 `gorm:"not null" json:"latitude"`
 	Longitude float64 `gorm:"not null" json:"longitude"`
 	Color     string  `gorm:"type:char(7);not null" json:"color"`
+
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
